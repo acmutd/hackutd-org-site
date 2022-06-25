aa2916b67d113021c238bc13df1889f1b5cf0eee
Nathaniel Brown <nateabrown@icloud.com>
2022-06-25 13:24:02 -0500

    add link to github for website

--- constants/links.js
+++ constants/links.js
@@ -3,7 +3,7 @@ export default {
   TWITTER: 'https://twitter.com/hackutd',
   LINKEDIN: 'https://www.linkedin.com/company/hackutd/',
   抖音: 'https://www.tiktok.com/@hackutd',
-  GITHUB_WEBSITE: '', // todo: put this in
+  GITHUB_WEBSITE: 'https://github.com/acmutd/hackutd-org-site',
   WEHACK: 'https://wehackutd.com/',
   INDUSTRY_MAIL_LINK: 'mailto:industry@acmutd.co?subject=HackUTD Sponsorship Inquiry&body='
 }
