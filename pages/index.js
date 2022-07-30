32a262742a2133e11cee26f6d171b6df3e6b869d
Nathaniel Brown <nateabrown@icloud.com>
2022-07-30 17:21:10 -0500

    add meta tag for imessage previews

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ export default function Home() {
     <Head>
       <title>HackUTD</title>
       <meta name="description" content="HackUTD is Texas' largest university-run hackathon." />
+      
+      {/* Primarily for Apple iMessage previews. OG only works with an absolute path. 
+          TODO: use a solution that generates this dynamically instead of requiring a weirdly specific link. */}
+      <meta property="og:image" content={"https://hackutd.co/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdefault.8bf97b58.jpeg&w=1920&q=75"} />
     </Head>
     <Main />
     </>
