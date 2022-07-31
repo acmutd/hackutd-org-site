279e4f1526e2051cbc2d725d4d2a9a4114103214
Nathaniel Brown <nateabrown@icloud.com>
2022-07-31 10:27:23 -0500

    fix script in head complaint

--- pages/_app.js
+++ pages/_app.js
@@ -29,32 +29,28 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      {/*
+        Adding analytics.
+        Reference:
+        * https://blog.avneesh.tech/add-google-analytics-to-your-nextjs-app
+      */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
-        {/*
-          Adding analytics.
-          Reference:
-          * https://blog.avneesh.tech/add-google-analytics-to-your-nextjs-app
-          * https://blog.avneesh.tech/add-google-analytics-to-your-nextjs-app
-        */}
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
-
       </Head>
       <GlobalStyle 
         dark={dark} 
