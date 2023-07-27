8e6cd284eaf4f8667a84615d589e9177231dceed
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-07-27 18:44:36 -0500

    Update officers

--- constants/officers.js
+++ constants/officers.js
@@ -1,16 +1,5 @@
 
 // probably a very inefficient method
-import vishvak_1 from 'assets/img/headshots/Vishvak-1.jpeg';
-import vishvak_2 from 'assets/img/headshots/Vishvak-2.jpeg';
-
-import chai_1 from 'assets/img/headshots/Chai-1.jpeg';
-import chai_2 from 'assets/img/headshots/Chai-2.jpeg';
-
-import ishpreet_1 from 'assets/img/headshots/Ishpreet-1.jpeg';
-import ishpreet_2 from 'assets/img/headshots/Ishpreet-2.jpeg';
-
-import abhitej_1 from 'assets/img/headshots/Abhitej-1.jpeg';
-import abhitej_2 from 'assets/img/headshots/Abhitej-2.jpeg';
 
 import abdullah_1 from 'assets/img/headshots/Abdullah-1.jpeg';
 import abdullah_2 from 'assets/img/headshots/Abdullah-2.jpeg';
@@ -21,31 +10,12 @@ import nam_2 from 'assets/img/headshots/Nam-2.jpeg';
 import dylan_1 from 'assets/img/headshots/Dylan-1.jpeg';
 import dylan_2 from 'assets/img/headshots/Dylan-2.jpeg';
 
-import arjun_1 from 'assets/img/headshots/Arjun-1.jpeg';
-import arjun_2 from 'assets/img/headshots/Arjun-2.jpeg';
-
-// import sameer_1 from 'assets/img/headshots/Sameer-1.jpeg';
-
-import aadit_1 from 'assets/img/headshots/Aadit-1.jpeg';
-import aadit_2 from 'assets/img/headshots/Aadit-2.jpeg';
-
-import neal_1 from 'assets/img/headshots/Neal-1.jpeg';
-import neal_2 from 'assets/img/headshots/Neal-2.jpeg';
-
-import shreya_1 from 'assets/img/headshots/Shreya-1.jpeg';
-import shreya_2 from 'assets/img/headshots/Shreya-2.jpeg';
-
 import mike_1 from 'assets/img/headshots/Mike-1.jpeg';
 import mike_2 from 'assets/img/headshots/Mike-2.jpeg';
 
 import parth_1 from 'assets/img/headshots/Parth-1.jpeg';
 import parth_2 from 'assets/img/headshots/Parth-2.jpeg';
 
-import nate_1 from 'assets/img/headshots/Nate-1.jpeg';
-import nate_2 from 'assets/img/headshots/Nate-2.jpeg';
-
-import aditya_1 from 'assets/img/headshots/Aditya-1.jpeg';
-import aditya_2 from 'assets/img/headshots/Aditya-2.jpeg';
 
 import esmond_1 from 'assets/img/headshots/Esmond-1.jpeg';
 import esmond_2 from 'assets/img/headshots/Esmond-2.jpeg';
@@ -59,85 +29,83 @@ import michael_2 from 'assets/img/headshots/Michael-2.jpeg';
 import purva_1 from 'assets/img/headshots/Purva-1.jpeg';
 import purva_2 from 'assets/img/headshots/Purva-2.jpeg';
 
-import shreyak_1 from 'assets/img/headshots/ShreyaK-1.jpeg';
-import shreyak_2 from 'assets/img/headshots/ShreyaK-2.jpeg';
-
-import anh_1 from 'assets/img/headshots/Anh-1.jpeg';
-import anh_2 from 'assets/img/headshots/Anh-2.jpeg';
-
-import rees_1 from 'assets/img/headshots/Rees-1.jpeg';
-import rees_2 from 'assets/img/headshots/Rees-2.jpeg';
-
-import anjelica_1 from 'assets/img/headshots/Anjelica-1.jpeg';
-import anjelica_2 from 'assets/img/headshots/Anjelica-2.jpeg';
-
 import muskaan_1 from 'assets/img/headshots/Muskaan-1.jpeg';
 import muskaan_2 from 'assets/img/headshots/Muskaan-2.jpeg';
-
-import neha_1 from 'assets/img/headshots/Neha-1.jpeg';
-import neha_2 from 'assets/img/headshots/pool_throw_small.gif';
-
-import angela_1 from 'assets/img/headshots/Angela-1.jpeg';
-import angela_2 from 'assets/img/headshots/Angela-2.jpeg';
-
-import lily_1 from 'assets/img/headshots/Lily-1.jpeg';
-import lily_2 from 'assets/img/headshots/Lily-2.jpeg';
-
-import tirzah_1 from 'assets/img/headshots/Tirzah-1.jpeg';
 
 import nick from 'assets/img/headshots/nick-yoshi.gif';
 
 import salman_1 from 'assets/img/headshots/Salman-1.jpeg';
 import salman_2 from 'assets/img/headshots/Salman-2.jpeg';
 
-import anna_1 from 'assets/img/headshots/Anna-1.jpeg';
-
 import avery_1 from 'assets/img/headshots/Avery-1.jpeg';
 
-import kelly_1 from 'assets/img/headshots/Kelly-1.jpeg';
-import kelly_2 from 'assets/img/headshots/Kelly-2.jpeg';
+import kelly_1 from 'assets/img/headshots/KellyZ-1.jpeg';
 
-import rishi_1 from 'assets/img/headshots/Rishi-1.jpeg';
-import rishi_2 from 'assets/img/headshots/Rishi-2.jpeg';
 
 import mitchell_1 from 'assets/img/headshots/Mitchell-1.jpeg';
 
+import adelaide_1 from 'assets/img/headshots/AdelaideD-1.jpeg';
+
+import bhuvana_1 from 'assets/img/headshots/BhuvanaM-1.jpeg';
+
+import farhan_1 from 'assets/img/headshots/FarhanJ-1.jpeg';
+import farhan_2 from 'assets/img/headshots/FarhanJ-2.jpeg';
+
+import kevin_1 from 'assets/img/headshots/KevinG-1.jpeg';
+import kevin_2 from 'assets/img/headshots/KevinG-2.jpeg';
+
+import shoaib_1 from 'assets/img/headshots/ShoaibH-1.jpeg';
+import shoaib_2 from 'assets/img/headshots/ShoaibH-2.jpeg';
+
+import lisa_1 from 'assets/img/headshots/LisaW.jpeg';
+
+import josh_1 from 'assets/img/headshots/JoshD-1.jpeg';
+import josh_2 from 'assets/img/headshots/JoshD-2.jpeg';
+
+import shaswat_1 from 'assets/img/headshots/ShazK-1.jpeg';
+import shaswat_2 from 'assets/img/headshots/ShazK-2.jpeg';
+
+
+import geeth_1 from 'assets/img/headshots/GeethG-1.jpeg';
+import geeth_2 from 'assets/img/headshots/GeethG-2.jpeg';
+
+import shreyaK_1 from 'assets/img/headshots/ShreyaK-1.jpeg';
+import shreyaK_2 from 'assets/img/headshots/ShreyaK-2.jpeg';
+
+
+
 
 export default [
-  { name: 'Vishvak Bandi', team: 'Director', pic: [vishvak_1, vishvak_2] },
-  { name: 'Chaithu Dikkala', team: 'Logistics', pic: [chai_1, chai_2] },
-  { name: 'Ishpreet Bhasin', team: 'Experience', pic: [ishpreet_1, ishpreet_2] },
-  { name: 'Abhitej Arora', team: 'Industry', pic: [abhitej_1, abhitej_2] },
-  { name: 'Abdullah Hasani', team: 'Tech', pic: [abdullah_1, abdullah_2] },
+  { name: 'Michael Zhao', team: 'Director', pic: [michael_1, michael_2] },
+  { name: 'Abdullah Hasani', team: 'Finance', pic: [abdullah_1, abdullah_2] },
   { name: 'Nam Truong', team: 'Tech', pic: [nam_1, nam_2] },
   { name: 'Dylan Nguyen', team: 'Marketing', pic: [dylan_1, dylan_2] },
-  { name: 'Arjun Venkat', team: 'Logistics', pic: [arjun_1, arjun_2] },
-  // { name: 'Sameer Haq', team: 'Tech', pic: [sameer_1] },
-  { name: 'Aadit Meenege', team: 'Industry', pic: [aadit_1, aadit_2] },
-  { name: 'Neal Gandhi', team: 'Logistics', pic: [neal_1, neal_2] },
-  { name: 'Shreya Parihar', team: 'Marketing', pic: [shreya_1, shreya_2] },
   { name: 'Mike Nguyen', team: 'Tech', pic: [mike_1, mike_2] },
+  { name: 'Nick Burnett', team: 'Tech', pic: [nick, nick] },
   { name: 'Parth Amrute', team: 'Industry', pic: [parth_1, parth_2] },
-  { name: 'Nate Brown', team: 'Marketing', pic: [nate_1, nate_2] },
-  { name: 'Aditya Rathod', team: 'Experience', pic: [aditya_1, aditya_2] },
-  { name: 'Esmond Tsang', team: 'Experience', pic: [esmond_1, esmond_2] },
-  { name: 'Caleb Lim', team: 'Logistics', pic: [caleb_1, caleb_2] },
-  { name: 'Michael Zhao', team: 'Experience', pic: [michael_1, michael_2] },
   { name: 'Purva Pawar', team: 'Industry', pic: [purva_1, purva_2] },
-  { name: 'Shreya Kuntumalla', team: 'Industry', pic: [shreyak_1, shreyak_2] },
-  { name: 'Anh Nguyen', team: 'Experience', pic: [anh_1, anh_2] },
-  { name: 'Rees Blatt', team: 'Logistics', pic: [rees_1, rees_2] },
-  { name: 'Anjelica Avorque', team: 'Marketing', pic: [anjelica_1, anjelica_2] },
+  { name: 'Caleb Lim', team: 'Logistics', pic: [caleb_1, caleb_2] },
+  { name: 'Shreya Kuntumalla', team: 'Industry', pic: [shreyaK_1, shreyaK_2] },
   { name: 'Muskaan Bansal', team: 'Logistics', pic: [muskaan_1, muskaan_2] },
-  { name: 'Angela Wang', team: 'Marketing', pic: [angela_1, angela_2] },
-  { name: 'Lily Vu', team: 'Marketing', pic: [lily_1, lily_2]},
-  // { name: 'Tirzah Lo', team: 'Tech', pic: [tirzah_1] },
-  { name: 'Salman Jaher', team: 'Marketing', pic: [salman_1, salman_2] },
-  { name: 'Anna Ernst', team: 'Experience', pic: [anna_1] },
-  // { name: 'Rishi Meka', team: 'Marketing', pic: [rishi_1, rishi_2] },
-  { name: 'Kelly Zhou', team: 'Marketing', pic: [kelly_1, kelly_2] },
-  { name: 'Avery Bainbridge', team: 'Experience', pic: [avery_1] },
+  { name: 'Esmond Tsang', team: 'Experience', pic: [esmond_1, esmond_2] },
+  { name: 'Avery Bainbridge', team: 'Experience', pic: [avery_1, avery_1] },
+  { name: 'Salman Jaher', team: 'Experience', pic: [salman_1, salman_2] },
+  { name: 'Kelly Zhou', team: 'Marketing', pic: [kelly_1,] },
+  { name: 'Daniel Liu', team: 'Experience', pic: [] },
   { name: 'Mitchell Vu', team: 'Marketing', pic: [mitchell_1] },
-  { name: 'Neha Rode', team: 'Former Director', pic: [neha_1, neha_2] },
-  { name: 'Nick', team: 'Tech', pic: [nick] }
+  { name: 'Geeth Gunnampalli', team: 'Tech', pic: [geeth_1, geeth_2] },
+  { name: 'Sharun Naicker', team: 'Industry', pic: [] },
+  { name: 'Farhan Jamil', team: 'Industry', pic: [farhan_1, farhan_2] },
+  { name: 'Kevin Ge', team: 'Experience', pic: [kevin_1, kevin_2] },
+  { name: 'Shoaib Huq', team: 'Marketing', pic: [shoaib_1, shoaib_2] },
+  { name: 'Adelaide (Addy) Dunning', team: 'Logistics', pic: [adelaide_1] },
+  { name: 'Bhuvana Madiraju', team: 'Industry', pic: [bhuvana_1] },
+  { name: 'Arjun Kumar', team: 'Logistics', pic: [] },
+  { name: 'Lisa Wu', team: 'Marketing', pic: [lisa_1] },
+  { name: 'Shagun  Dalal', team: 'Logistics', pic: [] },
+  { name: 'Amrit Rathie', team: 'Tech', pic: [] },
+  { name: 'Joshua (Josh) Das', team: 'Experience', pic: [josh_1, josh_2] },
+  { name: 'Shaswat (Shaz) Kumar', team: 'Marketing', pic: [shaswat_1, shaswat_2]},
+  { name: 'Abigail Smith', team: 'Logistics', pic: [] },
+  
 ]
