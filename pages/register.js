bb7874aeee8c413056013201afc7c04088da5bd2
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-09-18 18:29:16 -0500

    udpate /register redirect route to x.hackutd.co

--- pages/register.js
+++ pages/register.js
@@ -4,7 +4,7 @@ const { default: Link } = require("next/link");
 export default function Register() {
     return (
         <Head>
-            <meta httpEquiv="refresh" content="0; URL=https://ix.hackutd.co/" />
+            <meta httpEquiv="refresh" content="0; URL=https://x.hackutd.co/register" />
         </Head>
     )
 }
