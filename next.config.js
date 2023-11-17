24b9a244c6dcea378316059a596359d9c7632b8c
Michael Zhao <michaelzhao314@gmail.com>
2023-11-16 18:06:57 -0600

    Added redirects for apply and fixed register redirect w next tech

--- next.config.js
+++ next.config.js
@@ -3,13 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
   // allows hot reload for styled components to work correctly
   compiler: {
-    styledComponents: true
+    styledComponents: true,
   },
   // allows for browser language detection
   i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  }
-}
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  // redirects
+  async redirects() {
+    return [
+      {
+        source: "/register",
+        destination: "https://x.hackutd.co/register",
+        permanent: false,
+        basePath: false,
+      },
+      {
+        source: "/apply",
+        destination: "https://acmutd.typeform.com/to/K1IyS4Gj",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
