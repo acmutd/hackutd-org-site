e0b5ae232d442252cbb317ee3a1d0e00e1f99430
DedsecKnight <mikenguyen7268@gmail.com>
2024-07-28 21:39:39 -0500

    feat: added officer headshot for hackutd 24

--- components/team/OfficerView.js
+++ components/team/OfficerView.js
@@ -1,59 +1,57 @@
-import Image from "next/image"
-import styled from "styled-components"
+import Image from "next/image";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-    // flexy
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  // flexy
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    text-align: center;
-    font-size: 14px;
-    position: relative;
-`
+  text-align: center;
+  font-size: 14px;
+  position: relative;
+`;
 const Name = styled.div`
-    font-weight: 700;
-    margin-top: 5px;
-`
+  font-weight: 700;
+  margin-top: 5px;
+`;
 const Team = styled.div`
-    font-weight: 300;
-`
+  font-weight: 300;
+`;
 
 const Holder = styled.div`
-    position: relative;
-    border-radius: 20px;
-`
+  position: relative;
+  border-radius: 20px;
+`;
 
 const PlaceholderImage = styled.img`
-    width: 100%;
-    height: 100%;
-    border-radius: 20px;
-`
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+`;
 
 const NormalHolder = styled.div`
-    img {
-        border-radius: 20px;
-    }
-`
+  img {
+    border-radius: 20px;
+  }
+`;
 export default function OfficerView({ data, index }) {
-    return (
-        <Wrapper>
-            <Holder>
-                <NormalHolder>
-                    <Image
-                        src={(data.name === "Nick Burnett" ? require("assets/img/headshots/biggwiggle.gif") : require(`assets/img/headshots/${
-                            data.name.split(" ")[0]
-                        }.png`))}
-                        width={200}
-                        height={200}
-                        alt={`Image of ${data.name}, a HackUTD Officer.`}
-                        objectFit="cover"
-                    />
-                </NormalHolder>
-            </Holder>
+  return (
+    <Wrapper>
+      <Holder>
+        <NormalHolder>
+          <Image
+            src={require(`assets/img/headshots/${data.name.split(" ")[0]}.png`)}
+            width={200}
+            height={200}
+            alt={`Image of ${data.name}, a HackUTD Officer.`}
+            objectFit="cover"
+          />
+        </NormalHolder>
+      </Holder>
 
-            <Name>{data.name}</Name>
-            <Team>{data.team}</Team>
-        </Wrapper>
-    )
+      <Name>{data.name}</Name>
+      <Team>{data.team}</Team>
+    </Wrapper>
+  );
 }
